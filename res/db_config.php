72724d14b2bbed24e340e8827cfaec87f904427a
Adrian (1&1) <devilred101@gmail.com>
2012-05-19 14:14:48 +0200

    Updated the database configuration

--- res/db_config.php
+++ res/db_config.php
@@ -1,6 +1,6 @@
 <?php
-    $db_host="";
-    $db_user="";
-    $db_pass="";
-    $db_name="";
+    $db_host="db416074814.db.1and1.com";
+    $db_user="dbo416074814";
+    $db_pass="omgSQL";
+    $db_name="db416074814";
 ?>
