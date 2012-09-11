1983bbeeec657147077c0b84c5feb25f0568bd32
Adrian Reid <adrian@edtheloon.com>
2012-09-12 00:19:22 +0100

    Changed to use include method

--- register.php
+++ register.php
@@ -96,138 +96,63 @@
     <link rel="stylesheet" href="/stylesheets/default.css" type="text/css">
 </head>
 <body>
-    <!-- WRAPPER -->
-    <section id="wrap">
-        
-        <!-- BANNER -->
-        <section id="banner">
-            <header>
-                <h2>Banner</h2>
-            </header>
-            <p>A banner graphic goes here</p>  
-        </section>
-        <!-- END OF BANNER -->
-        
-        <!-- NAVIGATION -->
-        <nav id="nav">
-            <ul>
-                <li><a href="/home/">Home</a></li>
-                <li><a href="/blog/">Blog</a></li>
-                <li><a href="/follow/">Follow</a></li>
-                <li><a href="/projects/">Projects</a></li>
-                <li><a href="/about/">About</a></li>
-            </ul>
-        </nav>
-        <!-- END OF NAVIGATION -->
-        
-        <!-- MAIN CONTENT -->
-        <section id="main">            
-            <section class="post" style="text-align: center;">
-				<?php
-                    if (isset($_SESSION['err'])) {
-                        echo "<article><header><h1>Oops!</h1></header>" . $_SESSION['err'] . "</article>";
-					} else if (isset($_SESSION['success'])) {
-						echo "<article><header><h1>Success!</h1></header>" . $_SESSION['success'] . "</article>";
-                    } else {
-                        echo "<article><header><h1>Register</h1></header>Fill me in</article>";
-                    }
-                ?>
-				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <input name="username" type="text" placeholder="Username" maxlength="32" /><br>
-					<input name="email" type="email" placeholder="youremail@example.com" maxlength="255" /><br>
-					<input name="password" type="password" placeholder="Password" maxlength="32" /><br>
-					<input name="confirmpassword" type="password" placeholder="Confirm Password" maxlength="32" /><br>
-					<input name="submit" type="submit" value="Register" style="width: 158px; height: 30px;" onclick="return validateForm()" />
-				</form>
-				<?php
-					unset($_SESSION['err'], $_SESSION['success']);
-                ?>
-            </section>
-        </section>
-        <!-- END OF MAIN CONTENT -->
-        
-        <!-- SIDEBAR -->
-        <section id="sidebar">
-            <!-- USER CONTROLS -->
-            <nav class="sidebox">
-                <header>
-                    <h1>User Controls</h1>
-                </header>
-                <hr>
-                <ul>
-                    <li><a href="/login/">Login</a></li>
-                    <li><a href="/register/">Register</a></li>
-                    <li><a href="/submit/post/">Submit Post</a></li>
-                    <li><a href="/edit/profile/">Edit Profile</a></li>
-                    <li><a href="/edit/users/">Add/Edit Users</a></li>
-                    <li><a href="/logout/">Logout</a></li>
-                </ul>
-            </nav>
-            <!-- END OF USER CONTROLS -->
-            
-            <!-- WHO IS ONLINE -->
-            <div class="sidebox">
-                <header>
-                    <h1>Who's online</h1>
-                </header>
-                <hr>
-                <ul>
-                    <li>Adrian</li>
-                    <li>Daz</li>
-                    <li>Steve</li>
-                </ul>
-            </div>
-            <!-- END OF WHO IS ONLINE -->
-            
-            <!-- ADVERTISEMENT -->
-            <div class="sidebox" style="height: 600px;">
-                <header>
-                    <h1>Advertisement</h1>
-                </header>
-                <hr>
-            </div>
-            <!-- END OF ADVERTISEMENT -->
-        </section>
-        <!-- END OF SIDEBAR -->
-        
-        <!-- FOOTER -->
-        <footer id="footer">
-			Some copyright stuff | Ed the Loon | 2012
-		</footer>
-        <!-- END OF FOOTER -->
-        
-    </section>
-    <!-- END OF WRAPPER -->
-    
-    <!-- JAVASCRIPT -->
-    <script type="text/javascript">
-        function onFocus(el) {
-            if (el.value==el.defaultValue) {
-                el.value = el.defaultValue;
-            }
-        }
-        
-        function onBlur(el) {
-            if (el.value=='') {
-                el.value = el.defaultValue;
-            }
-        }
-        
-        function validateForm() {
-            var user = document.getElementsByName("username").item(0).value;
-            var email = document.getElementsByName("email").item(0).value;
-            var pass1 = document.getElementsByName("password").item(0).value;
-            var pass2 = document.getElementsByName("confirmpassword").item(0).value;
-            if (pass1 != pass2) {
-                alert("The passwords you have entered do not match!");
-                    el1[0].focus();
-                    el1[0].select();
-                    return false;
-            } else {
-                return true;
-            }
-        }
-    </script>
-    <!-- END OF JAVASCRIPT -->
+    <?php include "top.php"; ?>
+	<!-- MAIN CONTENT -->
+	<section id="main">            
+		<section class="post" style="text-align: center;">
+			<?php
+				if (isset($_SESSION['err'])) {
+					echo "<article><header><h1>Oops!</h1></header>" . $_SESSION['err'] . "</article>";
+				} else if (isset($_SESSION['success'])) {
+					echo "<article><header><h1>Success!</h1></header>" . $_SESSION['success'] . "</article>";
+				} else {
+					echo "<article><header><h1>Register</h1></header>Fill me in</article>";
+				}
+			?>
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+				<input name="username" type="text" placeholder="Username" maxlength="32" /><br>
+				<input name="email" type="email" placeholder="youremail@example.com" maxlength="255" /><br>
+				<input name="password" type="password" placeholder="Password" maxlength="32" /><br>
+				<input name="confirmpassword" type="password" placeholder="Confirm Password" maxlength="32" /><br>
+				<input name="submit" type="submit" value="Register" style="width: 158px; height: 30px;" onclick="return validateForm()" />
+			</form>
+			<?php
+				unset($_SESSION['err'], $_SESSION['success']);
+			?>
+		</section>
+	</section>
+	<!-- END OF MAIN CONTENT -->
+	<?php include "rest.php"; ?>
+
+	<!-- JAVASCRIPT -->
+	<script type="text/javascript">
+		function onFocus(el) {
+			if (el.value==el.defaultValue) {
+				el.value = el.defaultValue;
+			}
+		}
+		
+		function onBlur(el) {
+			if (el.value=='') {
+				el.value = el.defaultValue;
+			}
+		}
+		
+		function validateForm() {
+			var user = document.getElementsByName("username").item(0).value;
+			var email = document.getElementsByName("email").item(0).value;
+			var pass1 = document.getElementsByName("password").item(0).value;
+			var pass2 = document.getElementsByName("confirmpassword").item(0).value;
+			if (pass1 != pass2) {
+				alert("The passwords you have entered do not match!");
+					el1[0].focus();
+					el1[0].select();
+					return false;
+			} else {
+				return true;
+			}
+		}
+	</script>
+	<!-- END OF JAVASCRIPT -->
 </body>
 </html>
