38f15ee60d7e68449b780aec33921f1025ebbe43
Adrian Reid <adrian@edtheloon.com>
2012-09-12 00:17:20 +0100

    Added a template for adding new pages!
    Use this :D

---
+++ template.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="/stylesheets/default.css" type="text/css">
+</head>
+<body>
+	<?php include "top.php"; ?>
+    <!-- MAIN CONTENT -->
+	<section id="main">
+		<section class="post">
+			<article>
+				<header>
+					<h1>Title</h1>
+				</header>
+				<hr>
+				Content
+			</article>
+		</section>
+	</section>
+	<!-- END OF MAIN CONTENT -->
+    <?php include "rest.php"; ?>
+</body>
+</html>
