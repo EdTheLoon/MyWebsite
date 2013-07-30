ead638ee208c8a44fef64d5e8aa1b9ea7c6b3751
Adrian Reid <devilred101@gmail.com>
2013-07-31 00:30:59 +0100

    Delete db_config.php

--- res/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $db_host="hp109.hostpapa.com";
-    $db_user="edthe579_site";
-    $db_pass="icecream";
-    $db_name="edthe579_website";
-    $db_link = mysql_connect($db_host,$db_user,$db_pass) or die("Could not connect to mySQL: " . mysql_error());	
-    $db_salt = "omgSQL";
-    mysql_select_db($db_name,$db_link);
-?>
